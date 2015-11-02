e5be2bd22663454bf9a6cafc8e931bed523f6ea6
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2015-11-02 15:56:52 -0600

    add presentation module

---
+++ presentation/routes/config.js
@@ -0,0 +1,47 @@
+/*
+ *Tecnologico de Costa Rica
+ *Administracion de proyectos
+ *Carlos Fernandez Jimenez
+ *Frontend for TuristShareApp
+*/
+ 
+ /*
+  *Setter modulos touristShare
+ */
+ angular
+    .module('myTouristShare', [
+        'ngResource',
+        'ngRoute'
+    ]);
+    
+ /*Routes setter
+  *Define default routes for specified view
+ */
+(function(){
+    'use strict'
+    angular
+        .module('myTouristShare')
+        .config(function($locationProvider, $routeProvider){
+            $locationProvider
+                .html5Mode({
+                    enabled: true,
+                    requireBase: false
+                });
+            $routeProvider
+                .when('/',{
+                    templateUrl: 'views/login.html'
+                })
+                .when('/rooms',{
+                    templateUrl: 'views/rooms.html'
+                })
+                .when('/help',{
+                    templateUrl: 'views/help.html'
+                })
+                .when('/about',{
+                    templateUrl: 'views/about.html'
+                })
+                .otherwise({
+                    redirectTo: 'views/login.html'
+                });
+        });	
+})();
